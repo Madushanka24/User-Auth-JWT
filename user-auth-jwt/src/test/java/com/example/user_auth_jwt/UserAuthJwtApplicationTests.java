493d3b15dbe530ab6a7762dbf0bbc13ebb57bbab
Dillow <madushanka20021224@gmail.com>
2026-03-01 20:14:52 +0530

    dependencies configured

---
+++ user-auth-jwt/src/test/java/com/example/user_auth_jwt/UserAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_auth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

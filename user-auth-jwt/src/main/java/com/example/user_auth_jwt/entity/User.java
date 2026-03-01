c3b8fe5f9f06756e328c082d4fa3e9be324a662f
Dillow <madushanka20021224@gmail.com>
2026-03-01 20:19:31 +0530

    User Entity created

---
+++ user-auth-jwt/src/main/java/com/example/user_auth_jwt/entity/User.java
@@ -0,0 +1,62 @@
+package com.example.user_auth_jwt.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "USER";
+
+    public User() {
+    }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}

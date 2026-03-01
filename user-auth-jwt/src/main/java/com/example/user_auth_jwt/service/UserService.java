f941be2367bddfb0330644157465223d5a5cc801
Dillow <madushanka20021224@gmail.com>
2026-03-01 21:45:32 +0530

    UserService

--- user-auth-jwt/src/main/java/com/example/user_auth_jwt/service/UserService.java
+++ user-auth-jwt/src/main/java/com/example/user_auth_jwt/service/UserService.java
@@ -6,6 +6,7 @@ import com.example.user_auth_jwt.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
